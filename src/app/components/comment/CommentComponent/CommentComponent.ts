b2b5150ca8b5c636108f35faaa479c7aa64f1bfb
sklyukin <stas.msu@gmail.com>
2015-12-27 14:50:33 +0500

    avatar for comments

--- src/app/components/comment/CommentComponent/CommentComponent.ts
+++ src/app/components/comment/CommentComponent/CommentComponent.ts
@@ -3,17 +3,19 @@
 import {Component, Input} from 'angular2/core';
 import {Comment} from "../../../models/common/Comment";
 import {UserLink} from "../../user/UserLink/UserLink";
+import {UserAvatar} from "../../user/UserAvatar/UserAvatar";
 import {TimeAgoPipe} from 'angular2-moment';
 import {LikeComponent} from "../../like/LikeComponent/LikeComponent";
 
 @Component({
   selector: 'comment-component',
-  directives: [UserLink, LikeComponent],
+  directives: [UserLink, UserAvatar, LikeComponent],
   pipes: [TimeAgoPipe],
   styles: [require('./CommentComponent.scss')],
   template: require('./CommentComponent.html')
 })
 export class CommentComponent {
+  avatarSize = UserAvatar.SIZE.MINOR;
   @Input()
   comment:Comment;
 
--- src/app/components/user/UserAvatar/UserAvatar.ts
+++ src/app/components/user/UserAvatar/UserAvatar.ts
@@ -12,8 +12,18 @@ import {User} from "../../../models/common/User";
   template: require('./UserAvatar.html')
 })
 export class UserAvatar {
+  static SIZE = {
+    MINOR: 'minor',
+    MAIN: 'main'
+  };
+
   @Input()
   userId:string;
+
+  @Input()
+  size:string;
+
+  sizePx:number;
 
   user:User;
 
@@ -21,6 +31,20 @@ export class UserAvatar {
   }
 
   ngOnInit() {
+    this.setSizePx();
     this.userService.getById(this.userId).subscribe(user => this.user = user);
+  }
+
+  setSizePx() {
+    switch (this.size) {
+      case UserAvatar.SIZE.MINOR:
+        this.sizePx = 42;
+        break;
+      default:
+        //main
+        this.sizePx = 50;
+        break;
+    }
+
   }
 }
