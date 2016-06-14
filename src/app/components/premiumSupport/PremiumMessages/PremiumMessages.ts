a85e5d466277ebf6634a93b88c08ca9cbab514db
sklyukin <stas.msu@gmail.com>
2016-06-14 18:59:19 +0500

    premiumSupport removing messages

--- src/app/components/premiumSupport/PremiumMessages/PremiumMessages.ts
+++ src/app/components/premiumSupport/PremiumMessages/PremiumMessages.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import {ACL, PremiumSupport, UserService} from '@flaper/angular'
 
 @Component({
   selector: 'premium-messages',
@@ -9,6 +10,13 @@ export class PremiumMessages {
   @Input()
   messages = [];
 
-  constructor() {
+  constructor(private acl:ACL, private _user:UserService, private premiumSupport:PremiumSupport) {
+  }
+
+  removeMessage(message) {
+    if (confirm('Удалить сообщение?')) {
+      this.premiumSupport.del(message.id);
+      this.messages = this.messages.filter(m => m.id !== message.id)
+    }
   }
 }
