61db541d07885dda13156f3f2c138b362bd4c187
sklyukin <stas.msu@gmail.com>
2016-08-26 14:28:11 +0500

    комментарии

--- src/app/components/pages/object/LayoutObject/ObjectRoutes.ts
+++ src/app/components/pages/object/LayoutObject/ObjectRoutes.ts
@@ -19,6 +19,7 @@ export const ObjectRoutes =
       // тоже 'места'
       path: '%D0%BC%D0%B5%D1%81%D1%82%D0%B0/:region/:slug', component: LayoutObject,
       data: {mainDomain: 'места'}, children
-    }, //места
+    },
+    // остальные домены
     {path: ':mainDomain/:slug', component: LayoutObject, children},
   ];
