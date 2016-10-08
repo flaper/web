25b6e6e02c513f94470f02391215e8bbfeb18773
sklyukin <stas.msu@gmail.com>
2016-10-08 23:06:25 +0500

    autopaddings

--- src/app/components/pages/story/changes/PageStoryChanges.ts
+++ src/app/components/pages/story/changes/PageStoryChanges.ts
@@ -12,36 +12,39 @@ import * as Diff from "text-diff";
 })
 
 export class PageStoryChanges {
-  storyId: string;
+  storyId:string;
   history = [];
   dictionary = require('./i18n.json');
-  minimumDiffLength: number = 100;
-  currentPage: number = 1;
-  pageSize: number = 1;
-  pages: number = 0;
+  minimumDiffLength:number = 100;
+  currentPage:number = 1;
+  pageSize:number = 1;
+  pages:number = 0;
   diff;
-  constructor(private _story: StoryService, private route: ActivatedRoute,
-    private userService: UserService) {
+
+  constructor(private _story:StoryService, private route:ActivatedRoute,
+              private userService:UserService) {
     route.params.subscribe(params => {
       let {id, page} = params;     //story id & page number
       this.storyId = id;
       this.currentPage = parseInt(page) || 0;       // 0 = 1st page if page is not set
 
-      let query = { offset: this.currentPage * this.pageSize, limit: this.pageSize + 1 }, //building query for pagination
-        filter = { filter: JSON.stringify(query) };       // wrapping query into filter object
+      let query = {offset: this.currentPage * this.pageSize, limit: this.pageSize + 1}, //building query for pagination
+        filter = {filter: JSON.stringify(query)};       // wrapping query into filter object
       this.history = [];
       _story.getAudit(id, filter).subscribe(data => {  //requesting changes
         for (let i = 0; i < data.length - 1; i++) {
           userService.getById(data[i].userId).subscribe(user => {
-            this.history.push({ user: user, new: data[i], old: data[i + 1] });
+            this.history.push({user: user, new: data[i], old: data[i + 1]});
           })
         }
       });
     });
     this.diff = new Diff();
   }
+
   ngOnInit() {
   }
+
   getFields(record) {
     // let oldFields = Object.keys(record.old.fields),
     //     newFields = Object.keys(record.new.fields),
@@ -55,18 +58,19 @@ export class PageStoryChanges {
         oldValue = difference.filter(item => item[0] !== 1).map(item => item[0] === -1 ? `<span class='text-danger'>${item[1]}</span>` : item[1]).join("");
         newValue = difference.filter(item => item[0] !== -1).map(item => item[0] === 1 ? `<span class='text-success'>${item[1]}</span>` : item[1]).join("");
       }
-      return { key: field, oldValue: this.t(oldValue), newValue: this.t(newValue) };
+      return {key: field, oldValue: this.t(oldValue), newValue: this.t(newValue)};
     });
   }
 
   t(text) {
     return this.dictionary[text] ? this.dictionary[text] : text;
   }
+
   getPagination() {
-    let pages = [this.currentPage - 2, this.currentPage - 1, this.currentPage, this.currentPage + 1, this.currentPage + 2].filter(page => page > 0 && page <= this.pages);
-    return pages;
+    return [this.currentPage - 2, this.currentPage - 1, this.currentPage, this.currentPage + 1, this.currentPage + 2].filter(page => page > 0 && page <= this.pages);
   }
-  getRouterLink(page: any) {
+
+  getRouterLink(page:any) {
     switch (page) {
       case 'previous':
         return ['/p', 'storyChanges', this.storyId, (this.currentPage === 0 ? 0 : this.currentPage - 1)];
