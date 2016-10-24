5fce1fbc7445b7fb4e77b6a04b147c862373753c
HopeBetrayer <me.at.the.wind@gmail.com>
2016-10-24 14:10:29 +0300

    added return on menu button press

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -4,6 +4,7 @@ import {PageService} from "../../../services/helpers/PageService";
 import {Router,ActivatedRoute} from "@angular/router";
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ObjectSearchForm} from "../../object/ObjectSearchForm/ObjectSearchForm";
+import {Location} from "@angular/common";
 @Component({
   selector: 'navbar',
   styles: [require('./navbar.scss')],
@@ -12,7 +13,7 @@ import {ObjectSearchForm} from "../../object/ObjectSearchForm/ObjectSearchForm";
 export class Navbar {
   constructor(private authService:AuthService, private _user:UserService,
               private pageService:PageService, private router:Router,
-              private route:ActivatedRoute) {
+              private route:ActivatedRoute, private location:Location) {
   }
 
   logout() {
@@ -21,5 +22,12 @@ export class Navbar {
   searchFormVisible() {
     let path = window.location.pathname.split("/").filter(val => !!val);
     return path[0] != "o";
+  }
+  onMenuPress() {
+    let path = window.location.pathname.split("/").filter(val => !!val);
+    if (path.some(val => val === 'menu'))
+      this.location.back();
+    else
+      this.router.navigate(['/p','menu']);
   }
 }
