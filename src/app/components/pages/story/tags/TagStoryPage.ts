b1741373dc12d13864ee561c6c09cbd590b59eac
HopeBetrayer <me.at.the.wind@gmail.com>
2016-10-26 18:34:00 +0300

    added sort panel for news

--- src/app/components/pages/story/tags/TagStoryPage.ts
+++ src/app/components/pages/story/tags/TagStoryPage.ts
@@ -3,12 +3,16 @@ import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: "tag-story-page",
+  styles: [require("./TagStoryPage.scss")],
   template: require("./TagStoryPage.html")
 })
 
 export class TagStoryPage {
   where:any;
   tag:string;
+  sortType:number=0;
+  sortDirection:string = "DESC";
+  sortFields:string[] = ["lastActive","likesNumber","created"];
   constructor(private route:ActivatedRoute){
     route.params.subscribe(
       params => {
@@ -16,5 +20,16 @@ export class TagStoryPage {
         this.where = {tags:{inq:[this.tag]}}
       }
     )
+  }
+  sort(type:number) {
+    if (this.sortType === type) {
+        this.sortDirection = this.sortDirection === 'DESC' ? 'ASC' : 'DESC';
+    }
+    else {
+      this.sortType = type;
+    }
+  }
+  getSort() {
+    return `${this.sortFields[this.sortType]} ${this.sortDirection}`;
   }
 }
