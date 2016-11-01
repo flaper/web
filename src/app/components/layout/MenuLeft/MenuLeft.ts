857ac81f14bce5ee70b4645fb730b639cb741d3e
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-01 13:48:40 +0300

    moved menu generation to UserMenuService @flaper/angular

--- src/app/components/layout/MenuLeft/MenuLeft.ts
+++ src/app/components/layout/MenuLeft/MenuLeft.ts
@@ -1,5 +1,5 @@
-import {Component, ElementRef} from '@angular/core';
-import {ACL, AuthService, ObjectService, UserService} from "@flaper/angular";
+import {Component} from '@angular/core';
+import {AuthService, UserService,UserMenuService} from "@flaper/angular";
 import {PageService} from "../../../services/helpers/PageService";
 import {Router} from "@angular/router";
 let _get = require('lodash/get');
@@ -10,47 +10,14 @@ let _get = require('lodash/get');
   template: require('./MenuLeft.html')
 })
 export class MenuLeft {
-  items = [
-    {label: 'Баллы', route: ['/s', 'Баллы'], iconClass: 'fa fa-money'},
-    {label: 'Правила', route: ['/s', 'Правила'], iconClass: 'fa fa-info'},
-    {label: 'О проекте', route: ['/s', 'Флапер'], iconClass: 'fa fa-info-circle'}
-  ];
 
-  constructor(private _user:UserService, private acl:ACL, private elementRef:ElementRef, private _object:ObjectService,
-              private _page:PageService, private auth:AuthService, private router:Router) {
-  }
-  hasToggle() {
-    let item = ls.getItem('ml-toggle');
-    try {
-      item = JSON.parse(item);
-    }
-    catch (e) {
-      return false;
-    }
 
-    return item;
-  }
-
-  toggle() {
-    //mobile && tablets collapsed by default, desktop not
-    let el = this.elementRef.nativeElement.querySelector('.ml');
-    el.classList.toggle('ml-toggle');
-    ls.setItem('ml-toggle', el.classList.contains('ml-toggle'));
-  }
-
-  toggleSm() {
-    let el = this.elementRef.nativeElement.querySelector('#ml-items');
-    el.classList.remove('mobile-collapsed');
-    let $el:any = $(el);
-    $el.collapse('toggle');
+  constructor(private _user:UserService, private _auth:AuthService,
+              private _menu:UserMenuService,private _page:PageService) {
   }
 
   logout() {
-    this.auth.logout();
+    this._auth.logout();
   }
 
-  hasPremiumSupport() {
-    let premiumSupport = _get(this._user.currentUser, 'extra.premiumSupport', null);
-    return premiumSupport && premiumSupport > (new Date().toISOString());
-  }
 }
