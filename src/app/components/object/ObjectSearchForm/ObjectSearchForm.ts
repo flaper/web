997140ae1fcd53dc59f07dcc58a699c6c3e12599
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-21 14:53:55 +0300

    added domain and region fields to expanded search form

--- src/app/components/object/ObjectSearchForm/ObjectSearchForm.ts
+++ src/app/components/object/ObjectSearchForm/ObjectSearchForm.ts
@@ -1,6 +1,7 @@
 import {Component,ElementRef,Input} from "@angular/core"
-import {Router} from "@angular/router";
+import {Router,ActivatedRoute} from "@angular/router";
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {LocationService} from '@flaper/angular';
 
 @Component({
   selector: "object-search-form",
@@ -12,16 +13,30 @@ export class ObjectSearchForm {
   form:FormGroup;
   @Input()
   buttonMode:boolean = false;
+  @Input()
+  expand:boolean = false;
   constructor(private fb:FormBuilder,
-              private router:Router,
-              private el:ElementRef) {
-                let searchText = "";
-                this.form = this.fb.group({
-                  searchText: [searchText],
-                });
+              private router:Router, private route:ActivatedRoute,
+              private el:ElementRef, private _location:LocationService) {
+
+    this.form = this.fb.group({
+      searchText: [""],
+      domain: [_location.getCurrentDomain()],
+      region: [_location.getCurrentRegion()]
+    });
+  }
+  ngOnInit() {
+    this.route.params.subscribe(
+      params => {
+        let text = params["text"] || "";
+        this.form.controls['searchText'].setValue(text);
+      }
+    )
   }
   onSubmit(event) {
     let searchText = this.form.controls['searchText'].value.replace('/[^A-Za-zА-Яа-я!@#$,\. 0-9]/ig',"");
+    this._location.setCurrentRegion(this.form.controls['region'].value);
+    this._location.setCurrentDomain(this.form.controls['domain'].value);
     this.router.navigate(["/o",searchText]);
   }
 }
