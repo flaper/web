e7ffac97c235f9984391c925926350c3268bc806
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-21 13:00:51 +0300

    added related block to news page

--- src/app/components/pages/home/PageNews/Content/News.ts
+++ src/app/components/pages/home/PageNews/Content/News.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {LocationService,StoryService,Story} from '@flaper/angular';
 
 @Component({
   selector: 'news',
@@ -6,4 +7,24 @@ import {Component} from '@angular/core';
   styles: [require('./News.scss')]
 })
 export class News {
+  relatedStories:Story[] = [];
+  domain:string="";
+  region:string="";
+  relatedMessage:string="";
+  constructor(private _location:LocationService, private _story:StoryService) {
+    this.domain = _location.getCurrentDomain();
+    this.region = _location.getCurrentRegion();
+    let filter = {
+          where: this.domain === 'места' ? {domains:this.domain,region:this.region} : {domains:this.domain},
+          limit: 3,
+          offset: 0,
+          order: "created DESC"
+        };
+    if (this.domain)
+    _story.get(filter).subscribe(
+      stories =>  {
+        this.relatedStories = stories;
+      }
+    );
+  }
 }
