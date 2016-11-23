7888b81d3c8defd1f3377995d29713da3aeb088c
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-23 10:45:13 +0300

    added where parameter

--- src/app/modules/poll/PagePoll/PagePoll.ts
+++ src/app/modules/poll/PagePoll/PagePoll.ts
@@ -8,7 +8,7 @@ import {Component} from "@angular/core";
 
 export class PagePoll {
   order:string="created DESC";
-
+  where:any ={status:'active',or:[{type:"voting"},{type:"poll"}]};
   orderFields:any[] = [
     {name:"lastActive",title:"По дате последней активности"},
     {name:"created",title:"По дате создания"}
