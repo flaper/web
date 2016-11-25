5260088c9ecae10edb48c55e3fad13264a816467
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-25 19:58:04 +0300

    fixed random _location values change

--- src/app/components/object/ObjectSearchForm/ObjectSearchForm.ts
+++ src/app/components/object/ObjectSearchForm/ObjectSearchForm.ts
@@ -1,4 +1,4 @@
-import {Component,ElementRef,Input} from "@angular/core"
+import {Component,ElementRef,Input,Output,EventEmitter} from "@angular/core"
 import {Router,ActivatedRoute} from "@angular/router";
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {LocationService} from '@flaper/angular';
@@ -15,14 +15,15 @@ export class ObjectSearchForm {
   buttonMode:boolean = false;
   @Input()
   expand:boolean = false;
+  @Output()
+  onSearch:EventEmitter<boolean> =  new EventEmitter<boolean>();
   constructor(private fb:FormBuilder,
               private router:Router, private route:ActivatedRoute,
               private el:ElementRef, private _location:LocationService) {
-
     this.form = this.fb.group({
       searchText: [""],
-      domain: [_location.getCurrentDomain()],
-      region: [_location.getCurrentRegion()]
+      domain: [_location.getCurrentDomain() || ""],
+      region: [_location.getCurrentRegion() || ""]
     });
   }
   ngOnInit() {
@@ -35,8 +36,11 @@ export class ObjectSearchForm {
   }
   onSubmit(event) {
     let searchText = this.form.controls['searchText'].value.replace('/[^A-Za-zА-Яа-я!@#$,\. 0-9]/ig',"");
-    this._location.setCurrentRegion(this.form.controls['region'].value);
-    this._location.setCurrentDomain(this.form.controls['domain'].value);
+    if (this.expand) {
+      this._location.setCurrentRegion(this.form.controls['region'].value);
+      this._location.setCurrentDomain(this.form.controls['domain'].value);
+    }
     this.router.navigate(["/o",searchText]);
+    this.onSearch.emit(true);
   }
 }
