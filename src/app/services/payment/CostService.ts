ca0c2a8ebb7985785e4fbecc0cdcf60f45349c98
Stanislav Klyukin <stas.msu@gmail.com>
2017-09-23 22:33:11 +0500

    update prices and request

--- src/app/services/payment/CostService.ts
+++ src/app/services/payment/CostService.ts
@@ -1,9 +1,9 @@
 export class CostService {
-  static MANAGE_REQUEST = 300; // дисконт с 900р
-  static PREMIUM_SUPPORT = 420;
+  static MANAGE_REQUEST = 53400;
+  static PREMIUM_SUPPORT = 10720;
   static NO_INDEX = [
-    {name: '2years', amount: 11400, description: '2 года'},
-    {name: '5years', amount: 19400, description: '5 лет'},
-    {name: '10years', amount: 27600, description: '10 лет'},
+    {name: '2years', amount: 17100, description: '2 года'},
+    {name: '5years', amount: 29100, description: '5 лет'},
+    {name: '10years', amount: 41400, description: '10 лет'},
   ];
 }
